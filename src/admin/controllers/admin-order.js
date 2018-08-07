0be9312433911bd907c84999ab99493ed681ec20
Oscar Gracia <oscar.graciae@gmail.com>
2018-08-06 23:16:14 -0500

    Correccion de parametro de fecha en order/:date

--- src/admin/controllers/admin-order.js
+++ src/admin/controllers/admin-order.js
@@ -39,12 +39,18 @@ controller.getOrderDetail = async (req, res) => {
 
 controller.getAll = async (req, res) => {
   const { date } = req.params;
+  // const dateNow = moment(new Date(date), 'YYYY-MM-DD').format('YYYY-MM-DD');
+  // console.log("Fecha server-->", dateNow);
+  // const dateNow = moment(new Date(Date.now()), 'YYYY-MM-DD').format('YYYY-MM-DD');
   const yesterday = moment(date).add(1, 'days');
   const orders = await models.OrderDetail.findAll({
     where: {
       deliveryDate: {
+        // lt: new Date(),
+        // lt: date,
         lt: Date.parse(yesterday),
         gte: Date.parse(date),
+        // gte: Date.parse('2018-06-27'),
       },
     },
     include: [{ model: models.Order, include: [models.User] }, { model: models.Dish, as: 'dish' }],
--- src/admin/routes/admin-order.js
+++ src/admin/routes/admin-order.js
@@ -6,6 +6,6 @@ const routes = new Router();
 routes.get('/', orderController.getOrders);
 routes.get('/:id', orderController.getOrder);
 routes.get('/:id/detail', orderController.getOrderDetail);
-routes.get('/:date', orderController.getAll);
+routes.get('/:date/day', orderController.getAll);
 
 export default routes;
