d7517b168724d27be51efb017f858045aef2c493
Oscar Gracia <oscargracia@Oscars-MacBook-Pro.local>
2018-11-19 23:33:43 -0600

    Deploy

--- src/auth.js
+++ src/auth.js
@@ -14,6 +14,8 @@ export const createTokens = async (user) => {
     'secretpass',
   );
 
+
+
   console.log("Token", createToken);
   return createToken;
 };
@@ -62,7 +64,7 @@ export const refreshTokens = async (token, refreshToken, models, SECRET, SECRET2
     return {};
   }
 
-  const refreshSecret = user.password + SECRET2; 
+  const refreshSecret = user.password + SECRET2;
 
   try {
     jwt.verify(refreshToken, refreshSecret);
@@ -98,7 +100,7 @@ export const tryLogin = async (email, password, models, SECRET, SECRET2) => {
   const refreshTokenSecret = user.password + SECRET2;
 
   const [token, refreshToken] = await createTokens(user, SECRET, refreshTokenSecret);
-  
+
   return {
     ok: true,
     token,
